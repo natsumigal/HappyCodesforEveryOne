32988336270771c0c83bf0bfa465e003ea1a0d60
May Zuly <mayzuly.oo@outlook.com>
2024-09-12 11:19:09 +0700

    remove window form

--- ComfortHealthCare.Presentation/Pages/Patient.cshtml.cs
+++ ComfortHealthCare.Presentation/Pages/Patient.cshtml.cs
@@ -6,13 +6,14 @@ using System.Threading;
 using System.Threading.Tasks;
 using ApiClient.ComfortHealthApiClient;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace ComfortHealthCare.Presentation.Pages
 {
     public class PatientModel : PageModel
     {
         public List<PatientCommand> Patients { get; set; } = new List<PatientCommand>();
-        public List<DoctorCommand> Doctors { get; set; } = new List<DoctorCommand>();
+        public List<SelectListItem> Doctors { get; set; } = new List<SelectListItem>();
         public PatientCommand Patient { get; set; } = default!;
         private readonly ApiClient.ComfortHealthApiClient.ApiClient _apiClient;
         public int CurrentPage { get; set; } = 1;
@@ -36,19 +37,24 @@ namespace ComfortHealthCare.Presentation.Pages
             // Assuming the API provides a way to get the total count of doctors
             var totalCnt = await _apiClient.Gettotalcount2Async(new CancellationTokenSource().Token);
             TotalPages = (int)Math.Ceiling(Convert.ToDouble(totalCnt.Result) / (double)10);
+
+            // Call the method to populate the Doctors list
+            await OnGetDoctorIdandNameAsync();
         }
 
-        //public async Task OnGetDoctorAsync()
-        //{
-
-        //    var jsonString = await _apiClient.GetDoctor(pageNumber, new CancellationTokenSource().Token);
-        //    if (!string.IsNullOrEmpty(jsonString?.Result?.ToString()))
-        //    {
-        //        Patients = JsonConvert.DeserializeObject<List<PatientCommand>>(jsonString.Result.ToString()) ?? new List<PatientCommand>();
-        //    }
-
-        //    TotalPages = (int)Math.Ceiling(20 / (double)10);
-        //}
+        public async Task OnGetDoctorIdandNameAsync()
+        {
+            var jsonString = await _apiClient.GetdoctoridandnameAsync(new CancellationTokenSource().Token);
+            if (!string.IsNullOrEmpty(jsonString?.Result?.ToString()))
+            {
+                var doctorCommands = JsonConvert.DeserializeObject<List<DoctorCommand>>(jsonString.Result.ToString()) ?? new List<DoctorCommand>();
+                Doctors = doctorCommands.ConvertAll(d => new SelectListItem
+                {
+                    Value = d.Id.ToString(),
+                    Text = d.DoctorName
+                });
+            }
+        }
 
         public async Task<IActionResult> OnPostCreatePatientAsync(PatientCommand patient)
         {
